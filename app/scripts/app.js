c8a741c05268157967e5cee4cfc2a979d403cd45
Zemog <jgomez630@aol.com>
2017-05-02 21:36:47 -0700

    add html templates route and states

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,2 +1,28 @@
+(function() {
+     function config($stateProvider, $locationProvider) {
+      $locationProvider
+         .html5Mode({
+             enabled: true,
+             requireBase: false
+         });
+      $stateProvider
+         .state('landing', {
+             url: '/',
+             templateUrl: '/templates/landing.html'
+      })
+      .state('collection', {
+             url: '/collection',
+             templateUrl: '/templates/collection.html'
+      })
+         .state('album', {
+             url: '/album',
+             templateUrl: '/templates/album.html'
+     });     
+    }
+
 //    module  app-name  dependencies
-var app = angular.module('blocJams',[]);
+  angular
+    .module('blocJams',['ui.router'])
+    .config(config);
+
+})();
