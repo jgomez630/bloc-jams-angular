1b573aa89d173960b3dd107a9db23cae2cfb5a24
Zemog <jgomez630@aol.com>
2017-05-02 23:02:11 -0700

    checkpt 3-1 review

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,72 @@
-(function() {
+ (function() {
+     function config($stateProvider, $locationProvider) {
+       $locationProvider
+         .html5Mode({
+             enabled: true,
+             requireBase: false
+         });
+         
+       $stateProvider
+         .state('landing', {
+             url: '/',
+             templateUrl: '/templates/landing.html'
+         })
+          .state('album', {
+             url: '/album',
+             templateUrl: '/templates/album.html'
+         })
+       .state('collection', {
+             url: '/collection',
+             templateUrl: '/templates/collection.html'
+         });
+     }
+ 
+    angular
+       .module('blocJams', ['ui.router'])
+       .config(config);
+ })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*(function() {
      function config($stateProvider, $locationProvider) {
       $locationProvider
          .html5Mode({
@@ -25,4 +93,4 @@
     .module('blocJams',['ui.router'])
     .config(config);
 
-})();
+})(); */
